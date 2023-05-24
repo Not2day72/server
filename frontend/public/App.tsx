fb3e01ceeda25e99271c8fa79c3369c0cb5b07ad
Mikal bruinewoud <129078275+Not2day72@users.noreply.github.com>
2023-05-24 04:08:14 +0200

    Create App.tsx
    After install npm replace App.tsxx to display images.

---
+++ frontend/public/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import Clock from './Clock';
+import './App.css'
+
+const reactLogo = "react.svg"
+const viteLogo = "vite.svg"
+const djangoLogo = "django.svg"
+
+const reactLogoPath = `/frontend/static/${reactLogo}`
+const viteLogoPath = `/frontend/static/${viteLogo}`
+const djangoLogoPath = `/frontend/static/${djangoLogo}`
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogoPath} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogoPath} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://www.djangoproject.com/download/" target="_blank">
+          <img src={djangoLogoPath} className="logo django" alt="Django logo" />
+        </a>
+      </div>
+
+      <h1>Vite + React + Django</h1>
+      <Clock />
+      <p className="read-the-docs">
+        Click on the Vite, React and Django logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App

f423d33ab15ce3f51a02ea242351be3b43ce7230
Not2day72 <129078275+Not2day72@users.noreply.github.com>
2023-05-24 03:33:35 +0200

    Initial commit

---
+++ frontend/src/Clock.tsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h3>It is {time.toLocaleTimeString()}.</h3>
+    </div>
+  );
+}
+
+export default Clock;

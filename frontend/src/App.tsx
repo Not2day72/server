b5ad75e32911a5bec6cd2dab35ed040f1b1694aa
Not2day72 <129078275+Not2day72@users.noreply.github.com>
2023-05-24 13:00:39 +0200

    Updates App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,17 +17,17 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogoPath} className="logo" alt="Vite logo" />
+          <img src={viteLogoPath} className="logo" alt="Vite logo"  />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogoPath} className="logo react" alt="React logo" />
+        <img src={reactLogoPath} className="logo react" alt="React logo" />
         </a>
         <a href="https://www.djangoproject.com/download/" target="_blank">
           <img src={djangoLogoPath} className="logo django" alt="Django logo" />
         </a>
       </div>
-
-      <h1>Vite + React + Django</h1>
+      <div />
+      <h1>Vite + React + Django = ❤️</h1>
       <Clock />
       <p className="read-the-docs">
         Click on the Vite, React and Django logos to learn more
